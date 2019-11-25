cc7cf37189c3c2bd31b4d649df7b07a6ee3352b3
caofangqi <caofangqi@outlook.com>
2019-11-25 23:03:01 +0800

    create stock repository

---
+++ stock-helper/src/main/java/com/caofangqi/stock/stockhelper/StockHelperApplication.java
@@ -0,0 +1,13 @@
+package com.caofangqi.stock.stockhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockHelperApplication.class, args);
+    }
+
+}
---
+++ stock-helper/src/test/java/com/caofangqi/stock/stockhelper/StockHelperApplicationTests.java
@@ -0,0 +1,12 @@
+package com.caofangqi.stock.stockhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockHelperApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
