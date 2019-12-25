6184d64e971325c6c05794e174fcb77b6067112e
心河/曹方祺 <xinhe@webuy.ai>
2019-12-25 23:30:46 +0800

    新增okHttpUtils 新增 新浪行情地址

--- stock-helper/src/main/java/com/caofangqi/stock/stockhelper/StockHelperApplication.java
+++ stock-helper/src/main/java/com/caofangqi/stock/stockhelper/StockHelperApplication.java
@@ -1,7 +1,13 @@
 package com.caofangqi.stock.stockhelper;
 
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class StockHelperApplication {
@@ -9,5 +15,25 @@ public class StockHelperApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockHelperApplication.class, args);
     }
+
+
+    @Bean
+    public OkHttpClient okHttpClient() {
+        return new OkHttpClient.Builder()
+                //.sslSocketFactory(sslSocketFactory(), x509TrustManager())
+                .retryOnConnectionFailure(false)
+                .connectionPool(pool())
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30,TimeUnit.SECONDS)
+                .build();
+    }
+
+
+    @Bean
+    public ConnectionPool pool() {
+        return new ConnectionPool(200, 5, TimeUnit.MINUTES);
+    }
+
 
 }
---
+++ stock-helper/src/main/java/com/caofangqi/stock/stockhelper/constants/StockQuotationUrl.java
@@ -0,0 +1,21 @@
+package com.caofangqi.stock.stockhelper.constants;
+
+/**
+ * @author caofangqi
+ * @Created 2019/12/25 11:25 下午
+ */
+public interface StockQuotationUrl {
+    /**
+     * 新浪获取股票实时价格地址
+     * 例如获取以下几只股票价格
+     * http://hq.sinajs.cn/list=sh601006,sh601003,sh601001
+     *
+     */
+    static String SINA_STOCK_PRICE_URL = "http://hq.sinajs.cn/list=";
+
+
+
+
+
+
+}
