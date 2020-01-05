775ddeab85ec45eabb873c3ce2ee95644ca3b799
caofangqi <caofangqi@outlook.com>
2020-01-05 23:58:45 +0800

    调整下目录结构

---
+++ stock-helper/src/main/java/com/caofangqi/stock/stockhelper/domain/dto/StockDTO.java
@@ -0,0 +1,20 @@
+package com.caofangqi.stock.stockhelper.domain.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author caofangqi
+ * @Created 2019/12/26 11:10 下午
+ */
+@Data
+public class StockDTO implements Serializable {
+
+
+    private static final long serialVersionUID = -8152612667778392900L;
+
+
+
+
+}
